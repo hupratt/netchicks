36689fb03ea4ffd5b82fda794d0ccf80246f818f
Hugo Pratt <hprattdo@gmail.com>
2022-12-03 15:38:21 +0100

    responsive

--- js/loadDb.js
+++ js/loadDb.js
@@ -6,35 +6,14 @@ $(function () {
       var tblRow = `
 
         
-        <!-- Beasts of No Nation - OPEN -->
-        <div class="card carousel-cell">
+        <!-- ${f.title} - OPEN -->
+        <div class="card carousel-cell flex-box">
           <img
             class="card-img-top"
             src="media/images/posters/${f.thumbnail}"
           />
           <h5 class="card-title text-center">${f.title}</h5>
-          <div class="row">
-            <div class="col-3 text-left no-padding">2015</div>
-            <div class="col-3 text-center no-padding">
-              <a href="">
-                <img src="media/icons/eye.png" width="10" alt="" />
-              </a>
-            </div>
-            <div class="col-3 text-left no-padding">
-              <a href="">
-                <img src="media/icons/heart.png" width="10" alt="" />
-              </a>
-            </div>
-            <div class="col-3 text-right no-padding rating">
-              <img
-                src="media/icons/star.png"
-                width="10"
-                alt=""
-                class="padding-bottom-sm"
-              />
-              7.7
-            </div>
-          </div>
+
           <div class="overlay">
             <div class="text">
               <a
@@ -57,15 +36,15 @@ $(function () {
               </a>
             </div>
           </div>
-        <!-- Beasts of No Nation - OPEN -->
+        <!-- ${f.title} - CLOSE -->
 
     `;
 
       if (i % 4 === 0) {
         section++;
-        $(`<div class="card-deck section-` + section + `"></div>`).appendTo(
-          "#movies"
-        );
+        $(
+          `<div class="flex-boxes card-deck section-` + section + `"></div>`
+        ).appendTo("#movies");
       }
       $(tblRow).appendTo(".section-" + section);
     });
